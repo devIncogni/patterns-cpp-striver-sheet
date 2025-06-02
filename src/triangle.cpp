9de81079c689846b18b3b459282b3e921a6c58e4
devIncogni <arinkumar10@outlook.com>
2025-06-02 22:32:55 +0530

    Wrote a function to print a normal triangle

---
+++ src/triangle.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <iostream>
+void printTriangle(int height) {
+  int midIndex{static_cast<int>(std::ceil((2 * height + 1) / 2))};
+  for (int i = 0; i < height; ++i) {
+    for (int j{0}; j <= 2 * height + 1; ++j) {
+      int currentPrintNumber = 2 * i + 1;
+      int swing = (currentPrintNumber) / 2;
+      if (j >= midIndex - swing && j <= midIndex + swing) {
+        std::cout << "*";
+      } else {
+        std::cout << " ";
+      }
+    }
+    std::cout << "\n";
+  }
+}
+
+int main() { printTriangle(5); }
