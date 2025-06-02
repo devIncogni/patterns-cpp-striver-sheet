513af86f18fb8c6742d40ff7875757173a821645
devIncogni <arinkumar10@outlook.com>
2025-06-02 21:21:57 +0530

    wrote a function to print a binary right triangle

--- src/right_triangle.cpp
+++ src/right_triangle.cpp
@@ -38,10 +38,20 @@ void printInvertedRightTriangle(int height) {
 void printInvColNumRightTriangle(int height) {
   for (int i = 0; i < height; ++i) {
     for (int j = height; j > i; --j) {
-      std::cout << height-j+1;
+      std::cout << height - j + 1;
     }
     std::cout << '\n';
   }
 }
 
-int main() { printInvColNumRightTriangle(5); }
+void printBinaryRightTriangle(int height) {
+  int printValue{1};
+  for (int i = 0; i < height; ++i) {
+    for (int j = 0; j <= i; ++j) {
+      std::cout << printValue;
+      printValue = printValue == 0 ? 1 : 0;
+    }
+    std::cout << '\n';
+  }
+}
+int main() { printBinaryRightTriangle(5); }
