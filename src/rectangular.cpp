7d3d4027f7ae920761cf69884c0e102e0924b8f4
devIncogni <arinkumar10@outlook.com>
2025-06-02 20:27:19 +0530

    fixed bugs in printHollowRectangle()

--- src/rectangular.cpp
+++ src/rectangular.cpp
@@ -12,9 +12,10 @@ void printNormalRectangle(int height, int width) {
 void printHollowRectangle(int height, int width) {
   for (int i = 0; i < height; ++i) {
     for (int j = 0; j < width; ++j) {
-      std::cout << "*";
-      if ((i > 0 && j > 0) || (i < height && j < width)) {
-        continue;
+      if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
+        std::cout << "*";
+      } else {
+        std::cout << " ";
       }
     }
     std::cout << '\n';
@@ -22,6 +23,4 @@ void printHollowRectangle(int height, int width) {
 }
 
 // Testing purposes only main() function
-// int main(){
-//     printNormalRectangle(3, 4);
-// }
+int main() { printHollowRectangle(5, 6); }
